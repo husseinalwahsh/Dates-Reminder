7ee1f0b3c4875adb1d0232b36c6140a445f8c055
Hussein ALwhash <hussein.alwahsh02@gmail.com>
2025-03-05 12:55:22 +0200

    Project environment

---
+++ src/element/DatesReminder.jsx
@@ -0,0 +1,16 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+function DatesReminder() {
+  return (
+    <div>
+      {" "}
+      <Container fluid="md">
+        <Row>
+          <Col>1 of 1</Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default DatesReminder;
